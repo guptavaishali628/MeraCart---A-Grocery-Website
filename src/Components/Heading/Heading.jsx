2fbc71ec520d38dc9a1f802740eb29ad3b5453d3
vaishali <guptavaishali628@gmail.com>
2026-05-06 18:38:56 +0530

    created process component

--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = (props) => {
   return (
         <div className='w-fit mx-auto'>
-            <h2 className='md:text-5xl font-bold text-[2.5rem]'>
+            <h2 className='text-zinc-800 md:text-5xl font-bold text-[2.5rem]'>
                 <span className='text-orange-500'>{props.highlight}</span> {props.heading}
             </h2>
             <div className='w-34 h-1 bg-orange-300 md:mt-6 mt-3 ml-auto'></div>  {/* used to create line */}
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,6 +7,7 @@ import Category from '../Category/Category'
 import Values from '../Values/Values'
 import Products from '../Products/Products'
 import Discount from '../Discount/Discount'
+import Process from '../Process/Process'
 const Home = () => {
   return (
     <div>
@@ -16,6 +17,7 @@ const Home = () => {
       <Values/>
       <Products/>
       <Discount/>
+      <Process/>
     </div>
   )
 }
